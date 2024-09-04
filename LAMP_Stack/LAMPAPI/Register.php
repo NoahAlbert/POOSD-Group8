28ad8c98c635098ef404a5dced5d9c008bab48c2
JaceDaDorito <66852270+JaceDaDorito@users.noreply.github.com>
2024-09-03 20:23:46 -0400

    Register

---
+++ LAMP_Stack/LAMPAPI/Register.php
@@ -0,0 +1,42 @@
+<?php
+	$inData = getRequestInfo();
+	
+    $firstName = $inData["firstName"]
+    $lastName = $inData["lastName"]
+    $login = $inData["login"];
+	$password = $inData["password"];
+    $userId = $inData["userId"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Users (UserId, FirstName, LastName, Login, Password) VALUES(?,?,?,?,?)");
+		$stmt->bind_param("sssss", $userId, $firstName, $lastName, $login, $password, $userId);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
