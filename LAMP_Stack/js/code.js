ecde82f5dd733166b2b5865bc33a46623aa39be8
asjm223 <165121241+asjm223@users.noreply.github.com>
2024-09-12 23:58:03 -0400

    update code.js added deleteContact( )

--- LAMP_Stack/js/code.js
+++ LAMP_Stack/js/code.js
@@ -4,6 +4,7 @@ const extension = 'php';
 let userId = 0;
 let firstName = "";
 let lastName = "";
+const map1 = new Map();
 
 function doLogin()
 {
@@ -188,6 +189,8 @@ function addContact()
 	{
 		document.getElementById("contactAddResult").innerHTML = err.message;
 	}
+
+	
 	
 }
 
@@ -196,9 +199,7 @@ function searchContact()
 	let srch = document.getElementById("searchText").value;
 	document.getElementById("contactSearchResult").innerHTML = "";
 	
-	let nameList = "";
-	let numberList = "";
-	let emailList = "";
+	let table = "";
 
 	let tmp = {search:srch,userId:userId};
 	let jsonPayload = JSON.stringify( tmp );
@@ -214,25 +215,32 @@ function searchContact()
 		{
 			if (this.readyState == 4 && this.status == 200) 
 			{
+				//Sets up the table and headers
+				table += `<table id="contactsTable">`;
+				table += `<tr id="header">`;
+				table += `<th style="width:30%;">Name</th>`;
+				table += `<th style="width:30%;">Phone Number</th>`;
+				table += `<th style="width:30%;">Email</th>`;
+				table += `<th style="width:10%;">Edit</th>`;
+				table += `</tr>`;
+
 				document.getElementById("contactSearchResult").innerHTML = "Contact(s) has been retrieved";
 				let jsonObject = JSON.parse( xhr.responseText );
 				
 				for( let i=0; i<jsonObject.results.length; i++ )
 				{	
-
-					nameList += `<p class="tableElement">${jsonObject.results[i]["Name"]}</p>`;
-					numberList += `<p class="tableElement">${jsonObject.results[i]["Phone"]}</p>`;
-					emailList += `<p class="tableElement">${jsonObject.results[i]["Email"]}</p>`;
-					if( i < jsonObject.results.length - 1 )
-					{
-						//nameList += "<br />\r\n";
-						//numberList += "<br />\r\n";
-						//emailList += "<br />\r\n";
-					}
+					table += `<tr>`;
+					table += `<td id="nameColumn${i}">${jsonObject.results[i]["Name"]}</td>`;
+					table += `<td id="phoneColumn${i}">${jsonObject.results[i]["Phone"]}</td>`;
+					table += `<td id="emailColumn${i}">${jsonObject.results[i]["Email"]}</td>`;
+					table += `<td><span id=editButtonSpan${i}><button id="editButton${i}" onClick=editContact(${i})>Edit</button></span>`;
+					table += `<span id="deleteButtonSpan${i}">`;
+					table += `<button id="deleteButton${i}" onClick=deleteContact(${i})>Delete</button></span></td>`;
+					table += `</tr>`;
+					map1.set(i, jsonObject.results[i]["ID"]);
 				}
-				document.getElementById("nameColumn").innerHTML = nameList;
-				document.getElementById("numberColumn").innerHTML = numberList;
-				document.getElementById("emailColumn").innerHTML = emailList;
+				table += `</table>`;
+				document.getElementById("contactTableSpan").innerHTML = table; 
 			}
 		};
 		xhr.send(jsonPayload);
@@ -242,4 +250,97 @@ function searchContact()
 		document.getElementById("contactSearchResult").innerHTML = err.message;
 	}
 	
+}
+
+
+function editContact(index) {
+	let oldName = document.getElementById(`nameColumn${index}`).innerHTML;
+	let oldPhone = document.getElementById(`phoneColumn${index}`).innerHTML;
+	let oldEmail = document.getElementById(`emailColumn${index}`).innerHTML;
+
+	document.getElementById(`nameColumn${index}`).innerHTML = `<input id="nameInput${index}" type="text" value=${oldName}></input>`;
+	document.getElementById(`phoneColumn${index}`).innerHTML = `<input id="phoneInput${index}" type="text" value=${oldPhone}></input>`;
+	document.getElementById(`emailColumn${index}`).innerHTML = `<input id="emailInput${index}" type="text" value=${oldEmail}></input>`;
+
+	document.getElementById(`editButtonSpan${index}`).innerHTML = `<button id="updateButton${index}" onClick=updateContact(${index})>Update</button>`;
+}
+
+function deleteContact(index) {
+	
+	let delete_name = document.getElementById(`nameColumn${index}`).innerHTML;
+	
+	let check_delete = confirm('Are You Sure You Want To Delete The Contact: ' + delete_name);
+
+	if (check_delete === true)
+	{
+
+		let tmp = {
+			id: map1.get(index)
+		};
+	
+		let jsonPayload = JSON.stringify( tmp );
+	
+		let url = urlBase + '/DeleteContacts.' + extension;
+	
+		let xhr = new XMLHttpRequest();
+		xhr.open("POST", url, true);
+		xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+		try
+		{
+			xhr.onreadystatechange = function() 
+			{
+				if (this.readyState == 4 && this.status == 200) 
+				{
+					searchContact();
+				}
+			};
+			xhr.send(jsonPayload);
+		}
+		catch(err)
+		{
+			console.log("Update Failed");
+		}	
+	}
+
+
+
+}
+
+function updateContact(index) {
+	let newName = document.getElementById(`nameInput${index}`).value;
+	let newPhone = document.getElementById(`phoneInput${index}`).value;
+	let newEmail = document.getElementById(`emailInput${index}`).value;
+
+	let tmp = {
+        name: newName,
+        phone: newPhone,
+        email: newEmail,
+        id: map1.get(index)
+    };
+	let jsonPayload = JSON.stringify( tmp );
+
+	let url = urlBase + '/UpdateContacts.' + extension;
+
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				document.getElementById(`nameColumn${index}`).innerHTML = newName;//`<td id="nameColumn${index}">${newName}</td>`; 
+				document.getElementById(`phoneColumn${index}`).innerHTML = newPhone;//`<td id="phoneColumn${index}">${newPhone}</td>`; 
+				document.getElementById(`emailColumn${index}`).innerHTML = newEmail;//`<td id="emailColumn${index}">${newEmail}</td>`; 
+
+				document.getElementById(`editButtonSpan${index}`).innerHTML = `<button id="editButton${index}" onClick=editContact(${index})>Edit</button>`;
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		console.log("Update Failed");
+	}	
 }
