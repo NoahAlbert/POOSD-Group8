d87a25656fcf7021096700d458fc8ceb2b3854d8
Joe Hoang <jahstudentedu@gmail.com>
2024-09-20 14:29:40 -0400

    Changed layout and look of contact table, also updated search bar

--- LAMP_Stack/js/code.js
+++ LAMP_Stack/js/code.js
@@ -215,31 +215,37 @@ function searchContact()
 		{
 			if (this.readyState == 4 && this.status == 200) 
 			{
-				//Sets up the table and headers
-				table += `<table id="contactsTable">`;
-				table += `<tr id="header">`;
-				table += `<th style="width:30%;">Name</th>`;
-				table += `<th style="width:30%;">Phone Number</th>`;
-				table += `<th style="width:30%;">Email</th>`;
-				table += `<th style="width:10%;">Edit</th>`;
-				table += `</tr>`;
+				table += `<div id=contactTableDiv>`;
+				table += `<h3>CONTACT TABLE</h3>`;
+
 
 				document.getElementById("contactSearchResult").innerHTML = "Contact(s) has been retrieved";
 				let jsonObject = JSON.parse( xhr.responseText );
 				
 				for( let i=0; i<jsonObject.results.length; i++ )
 				{	
-					table += `<tr>`;
-					table += `<td id="nameColumn${i}">${jsonObject.results[i]["Name"]}</td>`;
-					table += `<td id="phoneColumn${i}">${jsonObject.results[i]["Phone"]}</td>`;
-					table += `<td id="emailColumn${i}">${jsonObject.results[i]["Email"]}</td>`;
-					table += `<td><span id=editButtonSpan${i}><button id="editButton${i}" onClick=editContact(${i})>Edit</button></span>`;
-					table += `<span id="deleteButtonSpan${i}">`;
-					table += `<button id="deleteButton${i}" onClick=deleteContact(${i})>Delete</button></span></td>`;
-					table += `</tr>`;
+					table += `<span id=contactSpan${i}>`;
+					table += `<span id=nameSpan${i}>`;
+					table += `<p style="font-weight: bold;">Name:&nbsp;</p>`
+					table += `<p id="name${i}">${jsonObject.results[i]["Name"]}</p>`;
+					table += `</span>`;
+					table += `<span id=phoneSpan${i}>`;
+					table += `<p style="font-weight: bold;">Phone:&nbsp;</p>`
+					table += `<p id="phone${i}">${jsonObject.results[i]["Phone"]}</p>`;
+					table += `</span>`;
+					table += `<span id=emailSpan${i}>`;
+					table += `<p style="font-weight: bold;">Email:&nbsp;</p>`
+					table += `<p id="email${i}">${jsonObject.results[i]["Email"]}</p>`;
+					table += `</span>`;
+					table += `<span id=buttonSpan${i}>`;
+					table += `<button id="editButton${i}" onClick=editContact(${i})>Edit</button>`;
+					table += `<button id="deleteButton${i}" onClick=deleteContact(${i})>Delete</button>`;
+					table += `</span>`;
+					table += `</span>`;
 					map1.set(i, jsonObject.results[i]["ID"]);
 				}
-				table += `</table>`;
+				table += `</div>`;
+				console.log(table);
 				document.getElementById("contactTableSpan").innerHTML = table; 
 			}
 		};
@@ -254,20 +260,22 @@ function searchContact()
 
 
 function editContact(index) {
-	let oldName = document.getElementById(`nameColumn${index}`).innerHTML;
-	let oldPhone = document.getElementById(`phoneColumn${index}`).innerHTML;
-	let oldEmail = document.getElementById(`emailColumn${index}`).innerHTML;
+	let oldName = document.getElementById(`name${index}`).innerHTML;
+	let oldPhone = document.getElementById(`phone${index}`).innerHTML;
+	let oldEmail = document.getElementById(`email${index}`).innerHTML;
 
-	document.getElementById(`nameColumn${index}`).innerHTML = `<input id="nameInput${index}" type="text" value=${oldName}></input>`;
-	document.getElementById(`phoneColumn${index}`).innerHTML = `<input id="phoneInput${index}" type="text" value=${oldPhone}></input>`;
-	document.getElementById(`emailColumn${index}`).innerHTML = `<input id="emailInput${index}" type="text" value=${oldEmail}></input>`;
+	document.getElementById(`name${index}`).innerHTML = `<input id="nameInput${index}" type="text" value=${oldName}></input>`;
+	document.getElementById(`phone${index}`).innerHTML = `<input id="phoneInput${index}" type="text" value=${oldPhone}></input>`;
+	document.getElementById(`email${index}`).innerHTML = `<input id="emailInput${index}" type="text" value=${oldEmail}></input>`;
 
-	document.getElementById(`editButtonSpan${index}`).innerHTML = `<button id="updateButton${index}" onClick=updateContact(${index})>Update</button>`;
+	editButton = document.getElementById(`editButton${index}`);
+	editButton.innerHTML = "Save";
+	editButton.setAttribute("onClick", `updateContact(${index})`);
 }
 
 function deleteContact(index) {
 	
-	let delete_name = document.getElementById(`nameColumn${index}`).innerHTML;
+	let delete_name = document.getElementById(`name${index}`).innerHTML;
 	
 	let check_delete = confirm('Are You Sure You Want To Delete The Contact: ' + delete_name);
 
@@ -330,11 +338,13 @@ function updateContact(index) {
 		{
 			if (this.readyState == 4 && this.status == 200) 
 			{
-				document.getElementById(`nameColumn${index}`).innerHTML = newName;//`<td id="nameColumn${index}">${newName}</td>`; 
-				document.getElementById(`phoneColumn${index}`).innerHTML = newPhone;//`<td id="phoneColumn${index}">${newPhone}</td>`; 
-				document.getElementById(`emailColumn${index}`).innerHTML = newEmail;//`<td id="emailColumn${index}">${newEmail}</td>`; 
+				document.getElementById(`name${index}`).innerHTML = newName;
+				document.getElementById(`phone${index}`).innerHTML = newPhone;
+				document.getElementById(`email${index}`).innerHTML = newEmail;
 
-				document.getElementById(`editButtonSpan${index}`).innerHTML = `<button id="editButton${index}" onClick=editContact(${index})>Edit</button>`;
+				editButton = document.getElementById(`editButton${index}`);
+				editButton.innerHTML = "Edit";
+				editButton.setAttribute("onClick", `editContact(${index})`);
 			}
 		};
 		xhr.send(jsonPayload);
