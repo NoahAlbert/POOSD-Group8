a55d8336bb3a54efce630850f34875f4b676cadb
asjm223 <165121241+asjm223@users.noreply.github.com>
2024-09-26 00:42:58 -0400

    update code.js
    formatting

--- LAMP_Stack/js/code.js
+++ LAMP_Stack/js/code.js
@@ -414,33 +414,41 @@ function validSignup()
 			letter.classList.add("invalid");
 		}
 
-	// Validate capital letters
-	var upperCaseLetters = /[A-Z]/g;
-	if(myInput.value.match(upperCaseLetters)) {
-		capital.classList.remove("invalid");
-		capital.classList.add("valid");
-	} else {
-		capital.classList.remove("valid");
-		capital.classList.add("invalid");
-	}
+		// Validate capital letters
+		var upperCaseLetters = /[A-Z]/g;
+		if(myInput.value.match(upperCaseLetters))
+		{
+			capital.classList.remove("invalid");
+			capital.classList.add("valid");
+		} 
+		else 
+		{
+			capital.classList.remove("valid");
+			capital.classList.add("invalid");
+		}
 
-	// Validate numbers
-	var numbers = /[0-9]/g;
-	if(myInput.value.match(numbers)) {
-		number.classList.remove("invalid");
-		number.classList.add("valid");
-	} else {
-		number.classList.remove("valid");
-		number.classList.add("invalid");
-	}
+		// Validate numbers
+		var numbers = /[0-9]/g;
+		if(myInput.value.match(numbers))
+		{
+			number.classList.remove("invalid");
+			number.classList.add("valid");
+		}
+		else
+		{
+			number.classList.remove("valid");
+			number.classList.add("invalid");
+		}
 
-	// Validate length
-	if(myInput.value.length >= 8) {
-		length.classList.remove("invalid");
-		length.classList.add("valid");
-	} else {
-		length.classList.remove("valid");
-		length.classList.add("invalid");
-	}
+		// Validate length
+		if(myInput.value.length >= 8) {
+			length.classList.remove("invalid");
+			length.classList.add("valid");
+		} 
+		else
+		{
+			length.classList.remove("valid");
+			length.classList.add("invalid");
+		}
 	}
 }
